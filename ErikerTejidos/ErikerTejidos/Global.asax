69a394eec747305c1bd1207dcef74c2779a55a07
Brian <atayayis@gmail.com>
2024-10-16 18:43:24 -0500

    Creando clase

---
+++ ErikerTejidos/ErikerTejidos/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ErikerTejidos.Global" Language="C#" %>
---
+++ ErikerTejidos/ErikerTejidos/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ErikerTejidos.Startup))]
+namespace ErikerTejidos
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
